05a4c0c11e4d91f29cd0d646c876eea125fb9bf4
JordanCYX <2338257659@qq.com>
2023-09-19 20:18:34 +0800

    第一次提交

---
+++ src/test/java/com/example/firstnetty/FirstNettyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.firstnetty;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstNettyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
